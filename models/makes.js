3d5ba12545c4827648c077b419c35b2b7ca8fa9e
ianjonze <ian3001@btopenworld.com>
2019-01-07 17:25:25 +0000

    added models

---
+++ models/makes.js
@@ -0,0 +1,100 @@
+
+//ARRAYS FOR DROPDOWN MENUS
+
+//MANUFACTURER
+
+const makes = [
+	{ 
+		id: 1,
+		make:'Ford'
+	},
+ 	{
+		id: 2, 
+		make:'Volkswagen',
+	},
+	{
+		id: 3, 
+		make:'Renault',
+	},
+	{
+		id: 4, 
+		make:'Vauxhall',
+	},
+	{
+		id: 5,
+		make: 'Mercedes',
+	}
+]
+
+const models = [
+
+	// VAUXHALL
+
+	{
+		model: 'Vivaro SWB 2015+',
+		makeId: 4
+	},
+	{
+		model: 'Vivaro LWB 2015+',
+		makeId: 4
+	},
+	{
+		model: 'Vivaro SWB pre 2014',
+		makeId: 4
+	},
+	{
+		model: 'Vivaro LWB pre 2014',
+		makeId: 4
+	},
+
+	//RENAULT 3
+	{
+		model: 'Trafic SWB 2015+',
+		makeId: 3
+	},
+	{
+		model: 'Trafic LWB 2015+',
+		makeId: 3
+	},
+	{
+		model: 'Trafic SWB pre 2014',
+		makeId: 3
+	},
+	{
+		model: 'Trafic LWB pre 2014',
+		makeId: 3
+	},
+	//FORD ID : 1
+	{
+		model: 'Connect SWB 2014+',
+		makeId: 1
+	},
+	{
+		model: 'Connect LWB 2014+',
+		makeId: 1
+	},
+	{
+		model: 'Connect SWB pre 2014',
+		makeId: 1
+	},
+	{
+		model: 'Connect LWB pre 2014',
+		makeId: 1
+	},
+
+	//MERCEDES ID 5
+
+	{
+		model: 'Vito SWB',
+		makeId: 5
+	},
+
+	//VOLKSWAGEN
+
+	{
+		model: 'Transporter T5 T6',
+		makeId: 2
+	}
+]
+
+	
